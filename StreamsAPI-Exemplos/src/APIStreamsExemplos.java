35ffd16526181e85edc9b4e9a6ac1e0e4788b6e4
Gabriel Amorim <novais.amorim@gmail.com>
2017-05-24 15:25:16 -0300

    alterações para exemplo de uso do git

--- StreamsAPI-Exemplos/src/APIStreamsExemplos.java
+++ StreamsAPI-Exemplos/src/APIStreamsExemplos.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+// Classe para testar a API Streams
 public class APIStreamsExemplos {
 
 	public static void main(String[] args) {
